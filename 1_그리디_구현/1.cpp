686e3919faba15ce4c03c7054dcf5b83692d801d
Kyeongjin Kim <krkgj0526@naver.com>
2020-12-24 18:10:36 +0900

    201224

--- 1_그리디_구현/1.cpp
+++ 1_그리디_구현/1.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+// 그리디 & 구현 - 1. 거스름 돈
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int n = 0;
+int cnt = 0;
+int coins[4] = {500, 100, 50, 10};
+
 int main(void){
-    cout << "hello world" << endl;
+    cin >> n; // n을 입력받는다.
+    for(int i = 0; i < 4; i++) {
+        cnt += n / coins[i];
+        n %= coins[i];
+    }
+    cout << cnt << endl;
 }
---
+++ 1_그리디_구현/2.cpp
@@ -0,0 +1,17 @@
+// 그리디 $ 구현 - 2. 1이 될 때까지
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k = 0;
+
+int main(void) {
+    cin >> n >> k;
+    while (1) {
+        if (n >= k) {
+            if (n % k == 0) {
+                
+            }
+        }
+    }
+}
