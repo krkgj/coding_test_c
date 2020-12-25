47c203db6fd1e995bcfb14c1eb87faee71c72fdd
Kyeongjin Kim <krkgj0526@naver.com>
2020-12-25 16:36:07 +0900

    201225-1

--- 1_그리디_구현/2.cpp
+++ 1_그리디_구현/2.cpp
@@ -4,14 +4,37 @@
 using namespace std;
 
 int n, k = 0;
+int cnt = 0;
 
 int main(void) {
     cin >> n >> k;
+    // while(1) {
+    //     if(n >= k) {
+    //         if (n % k == 0) {
+    //             n /= k;
+    //             cnt++;
+    //         }
+    //         else if(n % k != 0){
+    //             n -= 1;
+    //             cnt++;
+    //         }
+    //     }
+    //     else {
+    //         n -= 1;
+    //         cnt++;
+    //         if (n == 1) break;
+    //     }
+    // }
     while (1) {
-        if (n >= k) {
-            if (n % k == 0) {
-                
-            }
-        }
+        int target = (n / k) * k;
+        cnt += (n - target);
+        n = target;
+
+        if(n < k) break;
+        cnt++;
+        n /= k;
     }
+    cnt += (n-1);
+    cout << n << endl;
+    cout << "연산 횟수 => " << cnt << endl;
 }
